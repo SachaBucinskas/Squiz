12ad40fa6fc6d14c28b2edeb8f1e2c80e76f7640
SachaBucinskas <sachascholar@gmail.com>
2021-07-15 14:56:13 +1000

    Added some error handling to squizData if the specified file doesn't exist

--- src/squizData.py
+++ src/squizData.py
@@ -6,14 +6,17 @@ import json
 import os.path
 
 def getQuestions(quizFile):
-    with open(os.path.join('data','quizes',quizFile+'.json'),'r') as tempFile:
-        questionsData = json.load(tempFile)["questions"]
-        tempFile.close()
-    return(questionsData)
+    if os.path.isfile(os.path.join('data','quizes','subscriptions.json')): 
+        with open(os.path.join('data','quizes',quizFile+'.json'),'r') as tempFile:
+            questionsData = json.load(tempFile)["questions"]
+            tempFile.close()
+        return(questionsData)
+    else:
+        raise ValueError(quizFile + ".json does not exist inside Data/Quizes.")
 
 
-questionData = getQuestions("template") 
-for i in questionData:
-    print(i["question"])
-    print(i["responses"])
-    print(i["answer"])
+#questionData = getQuestions("template") 
+#for i in questionData:
+#    print(i["question"])
+#    print(i["responses"])
+#    print(i["answer"])
