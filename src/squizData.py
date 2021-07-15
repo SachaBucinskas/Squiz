42fc305f0d2cfd16ff43086bde8b38f5e386cf91
SachaBucinskas <sachascholar@gmail.com>
2021-07-15 14:06:57 +1000

    Finished json template.

---
+++ src/squizData.py
@@ -0,0 +1,5 @@
+# Name:    squizData.py
+# Purpose: This is responsible for the handling of json files for squiz, 
+#          including quizes (questions, answers), leaderboards, etc.
+
+import json
