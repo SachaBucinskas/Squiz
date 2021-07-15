6815556ef7d0c0011aff0de42d067545d36b204d
SachaBucinskas <sachascholar@gmail.com>
2021-07-15 19:59:36 +1000

    Added some functions to make printing boxes easier; Made the display out a bit more aesthetic

--- src/squizUI.py
+++ src/squizUI.py
@@ -8,9 +8,25 @@ import squizScoring
 
 controls = ["Q","W","E","R"]
 
+def clearScreen():
+    # Windows
+    if os.name == 'nt': os.system("cls")
+    # Mac/Linux/Posix
+    else: os.system("clear")
+
 def terminalWidth():
     width, height = os.get_terminal_size(0)
+    width = width - 2
     return(width)
+
+def printMiddleLine(line):
+    print("│" + line.center(terminalWidth()) + "│")
+
+def printTopLine():
+    print("┌" + ("─"*terminalWidth()) + "┐")
+
+def printBotLine():
+    print("└" + ("─"*terminalWidth()) + "┘")
 
 def spQuiz(questionsArg):
     questionNumber = 0 # For tracking which question currently on
@@ -19,21 +35,28 @@ def spQuiz(questionsArg):
 
      # Save terminal width and height so the formatting is correct & adjustable
 
-    for questionData in questionsArg:
-        print("=" * terminalWidth())
+    for questionData in questionsArg: # Loop over each question
+        clearScreen()
         questionNumber += 1
-        print(("Question: " + str(questionNumber) + "/" + str(totalQuestions)).center(terminalWidth()))
-        print(("Score: " + str(score)).center(terminalWidth()))
-        print("\n")
-        print(questionData["question"])
-        print("=" * terminalWidth())
+
+        printTopLine()
+        printMiddleLine(("Question: " + str(questionNumber) + "/" + str(totalQuestions)))
+        printMiddleLine(("Score: " + str(score)))
+        printBotLine()
+        printTopLine()
+        print(questionData["question"].center(terminalWidth()+2))
+        printBotLine()
+        
+
         responseNumber = 0
         longestResponse = 0
-        for response in questionData["responses"]:
+        for response in questionData["responses"]: # Get length of longest response for formatting
             if len(response) > longestResponse: longestResponse = len(response) 
         for response in questionData["responses"]:
             print(("[" + controls[responseNumber]+ "] " + response.center(longestResponse)).center(terminalWidth()))
             responseNumber += 1
         score += squizScoring.checkAnswer(getch.getch(), questionData["answer"])
+
+    print()
 
 spQuiz(squizData.getQuestions("template"))
